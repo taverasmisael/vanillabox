4245d50835d68231f8613da1fa18fe89dbb0a10f
taverasmisael <macrot29@gmail.com>
2018-04-15 10:55:56 -0400

    feat(build): optimize asstes for a library type outpug

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,38 +3,36 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   context: resolve(__dirname, './src'),
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   output: {
     path: resolve(__dirname, './dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'VanillaBox',
+    libraryTarget: 'umd'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: '[name].css',
-      allChunks: true
-    })
-  ],
   entry: './index.ts',
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: ['ts-loader', 'tslint-loader'] },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: ['babel-loader', 'ts-loader', 'tslint-loader']
+      },
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
         test: /\.(scss|sass)$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { importLoaders: 2, minimize: true, module: false, sourceMap: true }
-            },
-            { loader: 'postcss-loader', options: { sourceMap: true } },
-            { loader: 'sass-loader', options: { sourceMap: true } }
-          ]
-        })
+        use: [
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2, minimize: true, module: false, sourceMap: true }
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
       }
     ]
   }
